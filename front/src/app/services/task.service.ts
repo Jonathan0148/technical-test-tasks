e2cb490360e3fa84f74da838a92937c3529f626d
Jonathan Bohorquez Villamil <jonathan.bohorquez@TSICOL.LOC>
2025-03-18 11:12:14 -0500

    Proteccion rutas

--- front/src/app/services/task.service.ts
+++ front/src/app/services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { iTask } from '../models/task.model';
@@ -8,27 +8,32 @@ import { iTask } from '../models/task.model';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = `${environment.apiUrl}/data`;
+  private apiUrl = `${environment.apiUrl}/tasks`;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'x-api-key': environment.token
+    })
+  };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<iTask[]> {
-    return this.http.get<iTask[]>(this.apiUrl);
+    return this.http.get<iTask[]>(this.apiUrl, this.httpOptions);
   }
 
   getTaskById(id: number): Observable<iTask> {
-    return this.http.get<iTask>(`${this.apiUrl}/${id}`);
+    return this.http.get<iTask>(`${this.apiUrl}/${id}`, this.httpOptions);
   }
 
   createTask(data: iTask): Observable<iTask> {
-    return this.http.post<iTask>(this.apiUrl, data);
+    return this.http.post<iTask>(this.apiUrl, data, this.httpOptions);
   }
 
   updateTask(id: number, data: iTask): Observable<iTask> {
-    return this.http.put<iTask>(`${this.apiUrl}/${id}`, data);
+    return this.http.put<iTask>(`${this.apiUrl}/${id}`, data, this.httpOptions);
   }
 
   deleteTask(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`, { responseType: 'text' });
-  }  
+    return this.http.delete(`${this.apiUrl}/${id}`, { ...this.httpOptions, responseType: 'text' });
+  }
 }
